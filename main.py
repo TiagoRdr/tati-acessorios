f7fa93b33a26ab17af40405c7a1786404354a8de
TiagoRdr <50644359+TiagoRdr@users.noreply.github.com>
2024-12-18 13:42:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 
-st.set_page_config(layout="wide")
-
+st.set_page_config(layout="wide", theme={"base": "light"})
 # Streamlit Community: https://tati-aceapprios.streamlit.app/
 
 
